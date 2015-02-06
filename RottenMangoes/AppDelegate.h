f4a3656c4738d15963bbd0282b6d2c1441356040
Veronica Baldys <veronicabaldys@gmail.com>
2015-02-05 23:43:55 -0800

    Initial Commit

---
+++ RottenMangoes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RottenMangoes
+//
+//  Created by Veronica Baldys on 2015-02-05.
+//  Copyright (c) 2015 Veronica Baldys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RottenMangoes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RottenMangoes
+//
+//  Created by Veronica Baldys on 2015-02-05.
+//  Copyright (c) 2015 Veronica Baldys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
